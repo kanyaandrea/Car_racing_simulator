ac1780c9c706641388e7878c11a72e8166a0cc16
Andrea Kanya <andrea.kanya@gmail.com>
2017-08-17 11:46:31 +0200

    Car class created

---
+++ Motorcycle.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Motorcycle {
+    /*// speed is 100km/h. If rains, travels with 5-50km/h slower (randomly).
+    static nameNumber // The number of the instance created. Used for its name.
+            name // Are called "Motorcycle 1", "Motorcycle 2", "Motorcycle 3",... Unique.
+    distanceTraveled
+    moveForAnHour()*/
+}
+
+
